a0f78f04bea8377919cc77666e665ecd53ca2b84
Matias Schroder <matischroder@github.com>
2021-12-27 15:35:47 -0300

    go installation changed, sudo su

---
+++ go-update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+exit
--- main.sh
+++ main.sh
@@ -65,8 +65,8 @@ then
     printf "${GREEN}Go version $GO_VERSION is up to date\n${normal}...\n"
 else
     printf "${BLUE}Go version $GO_VERSION is outdated. Installing requiered version\n${normal}...\n"
-    wget -nc https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
-    sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+    wget -qO- https://go.dev/dl/go1.17.5.linux-amd64.tar.
+    sudo su -c ./go-update.sh root
     export PATH=$PATH:/usr/local/go/bin
     printf "${GREEN}Go version: $(go version)\n${normal}...\n"
 fi
