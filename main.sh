f3b91134d454098ad3808e7c4e041637a812a98f
Matias Schroder <matischroder@github.com>
2021-12-27 12:08:59 -0300

    changed go

--- main.sh
+++ main.sh
@@ -63,10 +63,9 @@ if [ $bool = 0 ]
 then
     printf "${GREEN}Go version $GO_VERSION is up to date\n${normal}...\n"
 else
-    sudo su
     printf "${BLUE}Go version $GO_VERSION is outdated. Installing requiered version\n${normal}...\n"
     wget https://go.dev/dl/go1.17.5.linux-amd64.tar.gz
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
+    sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.5.linux-amd64.tar.gz
     export PATH=$PATH:/usr/local/go/bin
     printf "${GREEN}Go version: $(go version)\n${normal}...\n"
 fi
